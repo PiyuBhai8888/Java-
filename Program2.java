2a6e55059b39dcafcf328198751022049c71c603
PiyuBhai8888 <piyushgirase26@gmail.com>
2026-01-06 16:56:42 +0530

    java fils

---
+++ Program2.java
@@ -0,0 +1,17 @@
+class Program2
+{
+public static void main(String [] args)
+{
+System.out.println("Name- Piyush Girase");
+System.out.println("Place- Pune");
+System.out.println("College- GH. Raisoni College");
+System.out.println("Degree- MCA, BCA");
+System.out.println("Hobbies- Clicket, Music");
+System.out.println("Language - Hindi,Marthi,English");
+System.out.println("Company- TCS, MasterCard, Wipro, Infosys ");
+System.out.println("Gender- Male");
+System.out.println("Number- 6260612397");
+
+}
+
+}
